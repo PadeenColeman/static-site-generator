038e922baf03cf4287577de91fcc249cb4e5c7f8
Padeen <ip4@posteo.de>
2026-06-20 20:55:59 +0200

    add main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
-./main.sh
-# hello world
+from textnode import TextNode, TextType
+
+
+def main():
+    inst = TextNode("Example text", TextType.BOLD, "www.duckduckgo.com")
+    print(inst)
+
+
+main()
