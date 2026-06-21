e553d1cf16d7b543adaed825712a3f714bf4ed82
Padeen <ip4@posteo.de>
2026-06-21 17:58:57 +0200

    update testhtmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -23,6 +23,17 @@ class TestHTMLNode(unittest.TestCase):
         self.assertEqual(node.__repr__(), node2)
         self.assertEqual(node3.__repr__(), node4)
 
+    def test_leaf_to_html_p(self):
+    node = LeafNode("p", "Hello, world!")
+    self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_b(self):
+    node = LeafNode("b", "world")
+    self.assertEqual(node.to_html(), "<b>world</b>")
+
+    def test_leaf_to_html_a(self):
+    node = LeafNode("a", "Hello, world!", {"href": "https://www.google.com"})
+    self.assertEqual(node.to_html(), "<a href=https://www.google.com>Hello, world!</a>")
 
 if __name__ == "__main__":
     unittest.main()
