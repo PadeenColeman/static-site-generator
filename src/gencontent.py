3831ec4d6d4deeef680052801b80154fced7b285
Padeen <ip4@posteo.de>
2026-07-26 17:16:50 +0200

    update main.py and gencontent.py

--- src/gencontent.py
+++ src/gencontent.py
@@ -26,3 +26,16 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(dest_dir, exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(template)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for entry in os.listdir(dir_path_content):
+        source_path = os.path.join(dir_path_content, entry)
+        dest_path = os.path.join(dest_dir_path, entry)
+
+        if os.path.isfile(source_path):
+            if source_path.endswith(".md"):
+                html_dest_path = dest_path[:-3] + ".html"
+                generate_page(source_path, template_path, html_dest_path)
+        else:
+            generate_pages_recursive(source_path, template_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from textnode import TextNode, TextType
 from copystatic import recursive_copy, delete_public_start_copy
-from gencontent import generate_page
+from gencontent import generate_pages_recursive
 
 
 def main():
     static_dir = "static"
     public_dir = "public"
+    dir_path_public = "./public"
+    dir_path_content = "./content"
+    template_path = "./template.html"
     delete_public_start_copy(static_dir, public_dir)
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 
 main()
