65652eed99740c921d6cf0e7181388e4afd51d3b
Padeen <ip4@posteo.de>
2026-07-26 15:49:47 +0200

    update gencontent.py

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,3 +1,4 @@
+import os
 from markdown_blocks import markdown_to_html_node
 
 
@@ -9,5 +10,19 @@ def extract_title(markdown):
     raise Exception("Sorry, no h1 header.")
 
 
-def generate_page():
-    pass
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        markdown = f.read()
+    with open(template_path) as f:
+        template = f.read()
+    node = markdown_to_html_node(markdown)
+    html = node.to_html()
+    title = extract_title(markdown)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+    dest_dir = os.path.dirname(dest_path)
+    if dest_dir != "":
+        os.makedirs(dest_dir, exist_ok=True)
+    with open(dest_path, "w") as f:
+        f.write(template)
