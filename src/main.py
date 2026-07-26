0b76d0d7feaf69ca17bcc075e884281385479576
Padeen <ip4@posteo.de>
2026-07-26 15:54:24 +0200

    update main.py

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from textnode import TextNode, TextType
 from copystatic import recursive_copy, delete_public_start_copy
+from gencontent import generate_page
 
 
 def main():
     static_dir = "static"
     public_dir = "public"
     delete_public_start_copy(static_dir, public_dir)
+    generate_page("content/index.md", "template.html", "public/index.html")
 
 
 main()
